e6adceda8139ea0788f03485c8ae5b1dc556d978
fbraza <fbraza@tutanota.com>
2020-10-01 20:59:27 +0100

    Part1 project done

--- userapi/src/routes/user.js
+++ userapi/src/routes/user.js
@@ -1,33 +1,42 @@
-const client = require('../dbClient')
+const express = require('express')
+const userController = require('../controllers/user')
 
-module.exports = {
-  create: (user, callback) => {
-    // Check parameters
-    if(!user.username) return callback(new Error("Wrong user parameters"), null)
-    // Create User schema
-    const userObj = {
-      firstname: user.firstname,
-      lastname: user.lastname,
-    }
-    // Check if username exists
-    client.exists(user.username, (err, res) => {
-        if (res === 1) {
-            return callback(new Error("Warning: the user already exists"), null)
-        } else {
-            client.hmset(user.username, userObj, (error, response) => {
-              if (err) return callback(error, null)
-              callback(null, response)
-          })
+const userRouter = express.Router()
+
+userRouter
+  .post('/', (req, resp) => {
+    userController.create(req.body, (err, res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
         }
+        return resp.status(400).json(respObj)
+      }
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(201).json(respObj)
     })
-  },
-  get: (username, callback) => {
-    client.hgetall(username, (error, response) => {
-      if (response) {
-        callback(null, response)
-        } else {
-          callback(new Error("Warning: The user does not exist"), null)
+  })
+  .get('/:username', (req, resp, next) => {
+    userController.get(req.params.username, (err, res) => {
+      let respObj
+      if(err) {
+        respObj = {
+          status: "error",
+          msg: err.message
         }
-      })
-    }
-  }
+        return resp.status(400).json(respObj)
+      }
+      respObj = {
+        status: "success",
+        msg: res
+      }
+      resp.status(201).json(respObj)
+    })
+  })
+
+module.exports = userRouter
