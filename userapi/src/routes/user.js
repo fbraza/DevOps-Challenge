8784760de2f8cb1209ca20353332ac6e949f89f7
fbraza <fbraza@tutanota.com>
2020-10-01 20:44:48 +0100

    user: add the get method

--- userapi/src/routes/user.js
+++ userapi/src/routes/user.js
@@ -1,29 +1,33 @@
-const express = require('express')
-const userController = require('../controllers/user')
+const client = require('../dbClient')
 
-const userRouter = express.Router()
-
-userRouter
-  .post('/', (req, resp) => {
-    userController.create(req.body, (err, res) => {
-      let respObj
-      if(err) {
-        respObj = {
-          status: "error",
-          msg: err.message
+module.exports = {
+  create: (user, callback) => {
+    // Check parameters
+    if(!user.username) return callback(new Error("Wrong user parameters"), null)
+    // Create User schema
+    const userObj = {
+      firstname: user.firstname,
+      lastname: user.lastname,
+    }
+    // Check if username exists
+    client.exists(user.username, (err, res) => {
+        if (res === 1) {
+            return callback(new Error("Warning: the user already exists"), null)
+        } else {
+            client.hmset(user.username, userObj, (error, response) => {
+              if (err) return callback(error, null)
+              callback(null, response)
+          })
         }
-        return resp.status(400).json(respObj)
-      }
-      respObj = {
-        status: "success",
-        msg: res
-      }
-      resp.status(201).json(respObj)
     })
-  })
-  // .get('/:username', (req, resp, next) => { // Express URL params - https://expressjs.com/en/guide/routing.html
-  //   // TODO Create get method API
-  //   const username = req.params.username
-  // })
-  
-module.exports = userRouter
+  },
+  get: (username, callback) => {
+    client.hgetall(username, (error, response) => {
+      if (response) {
+        callback(null, response)
+        } else {
+          callback(new Error("Warning: The user does not exist"), null)
+        }
+      })
+    }
+  }
