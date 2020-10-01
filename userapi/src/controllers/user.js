5ad008be49e65e878e57d7dfda7fac8c5793d294
fbraza <fbraza@tutanota.com>
2020-10-01 20:46:22 +0100

    controllers.user: add the get, check if contact already here

--- userapi/src/controllers/user.js
+++ userapi/src/controllers/user.js
@@ -3,21 +3,31 @@ const client = require('../dbClient')
 module.exports = {
   create: (user, callback) => {
     // Check parameters
-    if(!user.username)
-      return callback(new Error("Wrong user parameters"), null)
+    if(!user.username) return callback(new Error("Wrong user parameters"), null)
     // Create User schema
     const userObj = {
       firstname: user.firstname,
       lastname: user.lastname,
     }
-    // Save to DB
-    // TODO check if user already exists
-    client.hmset(user.username, userObj, (err, res) => {
-      if (err) return callback(err, null)
-      callback(null, res) // Return callback
+    // Check if username exists
+    client.exists(user.username, (err, res) => {
+        if (res === 1) {
+            return callback(new Error("Warning: the user already exists"), null)
+        } else {
+            client.hmset(user.username, userObj, (error, response) => {
+              if (err) return callback(error, null)
+              callback(null, response)
+          })
+        }
     })
   },
-  // get: (username, callback) => {
-  //   // TODO create this method
-  // }
-}
+  get: (username, callback) => {
+    client.hgetall(username, (error, response) => {
+      if (response) {
+        callback(null, response)
+        } else {
+          callback(new Error("Warning: The user does not exist"), null)
+        }
+      })
+    }
+  }
